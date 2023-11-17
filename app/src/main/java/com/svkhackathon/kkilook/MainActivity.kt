c0cf362a5138a016a6d9358c07b06bd6231f23a8
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-17 13:10:09 -0800

    Navigation drawer

--- app/src/main/java/com/svkhackathon/kkilook/MainActivity.kt
+++ app/src/main/java/com/svkhackathon/kkilook/MainActivity.kt
@@ -3,27 +3,35 @@ package com.svkhackathon.kkilook
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -44,52 +52,78 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                    val scope = rememberCoroutineScope()
-
-                    Scaffold(
-                        floatingActionButton = {
-                            ExtendedFloatingActionButton(
-                                text = { Text("Show drawer") },
-                                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                                onClick = {
-                                    scope.launch {
-                                        drawerState.apply {
-                                            if (isClosed) open() else close()
-                                        }
-                                    }
-                                }
-                            )
-                        }
-                    ) {
-                        ModalNavigationDrawer(
-                            drawerState = drawerState,
-                            drawerContent = {
-                                ModalDrawerSheet {
-                                    Text("Drawer title", modifier = Modifier.padding(16.dp))
-                                    Divider()
-                                    NavigationDrawerItem(
-                                        label = { Text(text = "Drawer Item") },
-                                        selected = false,
-                                        onClick = { /*TODO*/ }
-                                    )
-                                    // ...other drawer items
-                                }
-                            }
-                        ) {
-                            KkilookApp("Hello Kkilook!", it)
-                        }
-                    }
+                    AppScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun KkilookApp(name: String, paddingValues: PaddingValues) {
-
+private fun AppScreen() {
     val navController = rememberNavController()
+
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {},
+                navigationIcon = {
+                    Icon(
+                        Icons.Filled.Menu,
+                        contentDescription = "",
+                        modifier = Modifier.clickable {
+                            scope.launch {
+                                drawerState.apply {
+                                    if (isClosed) open() else close()
+                                }
+                            }
+                        })
+                })
+        }
+    ) {
+        Surface(modifier = Modifier.padding(it)) {
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent = {
+                    ModalDrawerSheet {
+                        Text(
+                            text = "Menu",
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                        Divider()
+                        NavigationDrawerItem(
+                            label = { Text(text = "Drawer Item") },
+                            selected = false,
+                            onClick = {
+                                navController.navigate(ROUTE_MAIN)
+                                scope.launch {
+                                    drawerState.apply {
+                                        close()
+                                    }
+                                }
+                            }
+                        )
+                        // ...other drawer items
+                    }
+                }
+            ) {
+                KkilookNavigation(navController = navController, "Hello Kkilook!", it)
+            }
+        }
+    }
+}
+
+@Composable
+fun KkilookNavigation(
+    navController: NavHostController,
+    name: String,
+    paddingValues: PaddingValues
+) {
 
     NavHost(navController = navController, startDestination = ROUTE_MAIN) {
         composable(ROUTE_MAIN) {
--- app/src/main/java/com/svkhackathon/kkilook/screen/MainScreen.kt
+++ app/src/main/java/com/svkhackathon/kkilook/screen/MainScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.svkhackathon.kkilook.KkilookApp
+import com.svkhackathon.kkilook.KkilookNavigation
 import com.svkhackathon.kkilook.ui.theme.KkilookTheme
 
 const val ROUTE_MAIN = "route_main"
@@ -43,6 +43,6 @@ fun MainScreen(
 @Composable
 fun GreetingPreview() {
     KkilookTheme {
-        KkilookApp("Android", PaddingValues(0.dp))
+        MainScreen("Android") {}
     }
 }
