a201c8d4e7db89c0bfc15dbd0d7b4be730a73675
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-17 23:41:31 -0800

    Add mock map

--- app/src/main/java/com/svkhackathon/kkilook/screen/MapScreen.kt
+++ app/src/main/java/com/svkhackathon/kkilook/screen/MapScreen.kt
@@ -1,9 +1,17 @@
 package com.svkhackathon.kkilook.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.svkhackathon.kkilook.R
 import com.svkhackathon.kkilook.ui.composable.ImageByUrl
 
 const val ROUTE_MAP = "route_map"
@@ -13,10 +21,11 @@ fun MapScreen(
 ) {
     Box(modifier = Modifier.fillMaxSize()) {
 
-        ImageByUrl(
-            url = "https://www.google.com/maps/d/thumbnail?mid=1JXudFgRmzNUU_cp6SY6X-Vgvl2o&hl=en",
+        Image(
+            painter = painterResource(id = R.drawable.map),
+            contentDescription = "map",
             modifier = Modifier.fillMaxSize(),
-            contentDescription = "map"
+            contentScale = ContentScale.Crop
         )
     }
 }
@@ -24,4 +33,10 @@ fun MapScreen(
 @Composable
 fun Pointer(modifier: Modifier) {
 
+}
+
+@Composable
+@Preview
+fun PreviewMapScreen() {
+    MapScreen()
 }
