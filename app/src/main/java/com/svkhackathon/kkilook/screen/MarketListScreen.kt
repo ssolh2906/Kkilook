abe55f6b0ad339bfc8941086d7e98e9d11ff1254
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-18 03:00:53 -0800

    Add filter UI

--- app/src/main/java/com/svkhackathon/kkilook/screen/MarketListScreen.kt
+++ app/src/main/java/com/svkhackathon/kkilook/screen/MarketListScreen.kt
@@ -1,7 +1,21 @@
 package com.svkhackathon.kkilook.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.svkhackathon.kkilook.ui.composable.MarketItem
 
 const val ROUTE_MARKET_LIST = "route_market_list"
@@ -17,10 +31,28 @@ fun MarketListScreen(onItemClick: () -> Unit) {
     val merchantList = listOf<MarketItemState>(
         item, item, item, item, item
     )
+    Column {
+        MarketTopBar()
 
-    LazyColumn {
-        items(merchantList.size) {
-            MarketItem(onItemClick = onItemClick)
+        LazyColumn {
+            items(merchantList.size) {
+                MarketItem(onItemClick = onItemClick)
+            }
+        }
+    }
+}
+
+@Composable
+fun MarketTopBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(24.dp)
+            .padding(horizontal = 12.dp)
+    ) {
+        Row(modifier = Modifier.align(Alignment.CenterEnd)) {
+            Icon(Icons.Default.ArrowDropDown, contentDescription = "sort")
+            Text(text = "Best Relevant", color = Color.Gray)
         }
     }
 }
