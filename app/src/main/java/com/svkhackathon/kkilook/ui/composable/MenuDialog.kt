3ebb18f94fe3c34b4ce1693dafd5d65a71fa119b
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2023-11-18 03:30:03 -0800

    menu item ui

--- app/src/main/java/com/svkhackathon/kkilook/ui/composable/MenuDialog.kt
+++ app/src/main/java/com/svkhackathon/kkilook/ui/composable/MenuDialog.kt
@@ -2,6 +2,7 @@ package com.svkhackathon.kkilook.ui.composable
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,6 +10,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
@@ -36,8 +38,13 @@ fun MenuDialog(
 
 @Composable
 fun MenuItem(text: String, onFilterChanged: (String) -> Unit, onDismissRequest: () -> Unit) {
-    Text(text = text, Modifier.clickable {
-        onDismissRequest()
-        onFilterChanged(text)
-    })
+    Text(text = text,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .padding(12.dp)
+            .clickable {
+                onDismissRequest()
+                onFilterChanged(text)
+            }
+    )
 }
